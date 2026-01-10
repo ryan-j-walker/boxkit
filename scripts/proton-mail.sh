720c3f4181a7e3c8b775662cc700918e164127f8
Ryan Walker <ryanwalker8484@gmail.com>
2026-01-10 17:49:19 -0500

    Adding -y to dnf install proton mail command

--- scripts/proton-mail.sh
+++ scripts/proton-mail.sh
@@ -6,6 +6,6 @@
 # Update the container and install packages
 dnf upgrade -y
 wget https://proton.me/download/mail/linux/ProtonMail-desktop-beta.rpm
-dnf install ./ProtonMail-desktop-beta.rpm
+dnf install -y ./ProtonMail-desktop-beta.rpm
 rm -f ProtonMail-desktop-beta.rpm
 grep -v '^#' ./proton-mail.packages | xargs dnf install -y
